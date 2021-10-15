365c5ccb50de0b0e08cad89f2c95d7855162ddd5
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-10-15 11:25:21 +0600

    firebase login update

--- src/components/Home/HeaderContent/HeaderContent.js
+++ src/components/Home/HeaderContent/HeaderContent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import frame from '../../../resources/images/header_background.jpg'
 
 const HeaderContent = () => {
@@ -6,12 +7,13 @@ const HeaderContent = () => {
         <div className="container mt-5">
             <div className="row">
                 <div className="col-md-4 col-sm-12">
-                    <h1 className="mb-5">We Build <br/>Your Dream</h1>
-                    <p style={{marginBottom:"5px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus commodo ipsum duis laoreet maecenas. Feugiat </p>
-                    <button className="btn btn-primary main-btn">Booking</button>
+                    <h1 className="mb-5">We Build <br />Your Dream</h1>
+                    <p style={{ marginBottom: "5px" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus commodo ipsum duis laoreet maecenas. Feugiat </p>
+                    {/* <button className="btn btn-primary main-btn">Booking</button> */}
+                    <Link to="/order/:name" ><button className="btn btn-primary main-btn">JOIN US</button></Link>
                 </div>
                 <div className="col-md-7 col-sm-12 offset-md-1">
-                    <img className="img-fluid" style={{width:'80%',borderRadius:'10px'}} src={frame} alt=""/>
+                    <img className="img-fluid" style={{ width: '80%', borderRadius: '10px' }} src={frame} alt="" />
                 </div>
             </div>
         </div>
--- src/components/Home/Login/firebase.config.js
+++ src/components/Home/Login/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDC0nkztPAAa0fomW3fAqRV9BDvQeSHavo",
-  authDomain: "krrishir-kotha.firebaseapp.com",
-  projectId: "krrishir-kotha",
-  storageBucket: "krrishir-kotha.appspot.com",
-  messagingSenderId: "86438407845",
-  appId: "1:86438407845:web:8aaec6b6eb1bd90da3a74c"
+  apiKey: "AIzaSyC3OIOtxFaVdMGAyXQZ7UDiuHW5AkXrbNI",
+  authDomain: "rock-s-gym-25c5f.firebaseapp.com",
+  projectId: "rock-s-gym-25c5f",
+  storageBucket: "rock-s-gym-25c5f.appspot.com",
+  messagingSenderId: "27941267908",
+  appId: "1:27941267908:web:042c4711f3c7a56b29dc91"
 };
 export default firebaseConfig;
